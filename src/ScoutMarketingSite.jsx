5e00dff1d9711a7f5d7fed5a4873d4fd9fb83549
Brian Bennett <bennettb15@gmail.com>
2026-01-28 18:19:31 -0500

    value fix

--- src/ScoutMarketingSite.jsx
+++ src/ScoutMarketingSite.jsx
@@ -894,7 +894,7 @@ async function handleContactSubmit(e) {
 
 
     {/* Right: bullets */}
-<div className="rounded-3xl border border-border bg-[var(--brand)]/5 p-5 md:-mt-24">
+<div className="rounded-3xl border border-border bg-[var(--brand)]/5 p-5 md:-mt-20">
 
 
 
